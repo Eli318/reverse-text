a1c862ddcb2b84a354ad3a9658080e031f8ba1f7
Eli318 <eliezerroseman@gmail.com>
2021-04-05 00:09:11 +0300

    first commit

---
+++ text.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  4 19:40:28 2021
+
+@author: Eliezer
+"""
+
+def revword(word:str):
+    return word.lower()[::-1]
+
+
+
+def countword():
+    f = open("C:/Users/Eliezer/Documents/introduction_to_data_sciense/assignment2/text.txt", "r")
+    words = f.read().replace("\n", " ").split(" ")
+    f.close()
+    
+    key_word = words[0]
+    counter = 1
+    
+    for word in words[1:]:
+        if revword(word) == key_word:
+            counter += 1
+    
+    
+    return counter
+
+countword()
